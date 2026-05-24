fe5c0c31407eaa07d692f32a8f038fe7c87e1038
SampleTrack <FirstSample@proton.me>
2026-05-24 18:49:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,3 +10,5 @@ class Config:
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
     PORT = int(os.getenv("PORT", "8080"))
     PREMIUM_PRICE_STARS = int(os.getenv("PREMIUM_PRICE_STARS", "250"))
+    # Add your personal Telegram User ID here:
+    OWNER_ID = int(os.getenv("OWNER_ID", "0")) 
